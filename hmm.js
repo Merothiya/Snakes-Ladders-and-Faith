da9bf44c2127bac9e007038e21ed96dc36b38b72
Sachin <101268072+Merothiya@users.noreply.github.com>
2022-12-19 17:45:50 +0530

    Update hmm.js
    game finally working , things to keep in mind
    1 snakes and ladders are of 1 unit only
    2 kabhi kabar shayad canvas see bahar jaara h player
    3 dice variable ni h instead simple counting h 1 to 5 pr shayad koi bug aari usme bhi

--- hmm.js
+++ hmm.js
@@ -5,17 +5,17 @@ canvas.height = window.innerHeight;
 
 
 
-
-var h = window.innerHeight / 20;
-var w = window.innerWidth / 30;
-var x = window.innerWidth / 3;
-var y = window.innerHeight / 4;
+let h = window.innerHeight / 20;
+let w = window.innerWidth / 30;
+let x = window.innerWidth / 3;
+let y = window.innerHeight / 4;
 
 
 
 playArr = [];
 var colArr = ["#ccd5ae", "#e9edc9", "#b7e4c7", "#faedcd", "#f4f1de"];
 function init() {
+
     var he = window.innerHeight / 20;
     var wi = window.innerWidth / 30;
     var xc = window.innerWidth / 3;
@@ -29,21 +29,21 @@ function init() {
         c.fillText(101 - i, xc + wi / 2, yc + he);
         xc += wi;
         if (i % 10 == 0) {
-            // var x = window.innerWidth / 3;
+
             yc += he;
-            // x = window.innerWidth - x;
+
             wi = -wi
 
 
         }
     }
-    
-    // Player.draw();
+
+
 
 }
 
-function createplayer(){
-playArr.push(new Player(x, 3*y));
+function createplayer() {
+    playArr.push(new Player(window.innerWidth / 3, 3 * window.innerHeight / 4));
 }
 
 
@@ -53,47 +53,45 @@ window.addEventListener('resize',
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
 
+        h = window.innerHeight / 20;
+        w = window.innerWidth / 30;
+        x = window.innerWidth / 3;
+        y = window.innerHeight / 4;
+
+
         init();
-        // update();
-        // store();
 
     })
 
-// init();
+
 
 document.getElementById('start').addEventListener("click", function startGame() {
-    // new createPlayer(x, 3 * y - h / 3);
-    // console.log(player);
+
     init();
     createplayer();
+    if (document.getElementById("dice").disabled) {
+        location.reload();
+        // startGame()
+
+    }
 });
-// console.log(s);
 
-// update(){
-
-// }
 
 
 
 
 document.getElementById('dice').addEventListener("click", function () {
-    // console.log("yahan se random number niklega ek <= 6");
+
     store();
     // update();
     // Player.update();
+    // outcome();
 });
 
-
-// canvas.y = y;
-
-// c.translate(x, y);
-// console.log(c);
-// c.font = "Comic Sans MS";
-
-
-// c.fillStyle = "red";
-// c.fillRect(x + w, y, w, h);
-// console.log(Event);
+function disable() {
+    document.getElementById("dice").disabled = true;
+    document.getElementById("start").innerText = "NEW GAME";
+}
 
 
 
@@ -102,6 +100,7 @@ document.getElementById('dice').addEventListener("click", function () {
 
 
 
+c.beginPath();
 
 function Player(abs, ord) {
     this.x = abs;
@@ -109,60 +108,139 @@ function Player(abs, ord) {
 
     this.draw = function () {
         init();
-        c.fillStyle = "red";
-        c.fillRect(this.x + w / 3, this.y - (2 * h / 3), w / 3, h / 3);
+
+        c.fillStyle = "green";
+        if (w > 0) {
+            c.fillRect(this.x + (w) / 3, this.y - (2 * h / 3), (w) / 3, h / 3);
+        }
+        else {
+            c.fillRect(this.x + (w) / 3, this.y - (2 * h / 3), w / 3, h / 3);
+
+        }
+        // c.fillStyle = "black";
+        // c.fillRect(((window.innerWidth / 3) + Math.abs(w)), (window.innerHeight / 4) + h, w / 3, h / 3);
+        // if ((this.y >= window.innerHeight / 4 + h && !(this.x == window.innerWidth / 3))) {
+        //     // c.beginPath();
+        //     // c.strokeStyle = "red";
+        //     // c.moveTo(this.x, this.y);
+        //     // c.lineTo(((Math.floor(Math.random() * 10)) * Math.abs(w)) + window.innerWidth / 3, -(Math.floor(Math.random() * 10)) * h + 3 * y);
+        //     // c.stroke();
+        // }
     }
 
-    
+
 
     this.draw();
-    // console.log(playArr[0]);
+
 }
 
 
 
-
-
-
-//
-// //
+var ct = 0;
 function store() {
-    // = Player();
-    // update();
-    // playArr[0].update();
-    // playArr[0].draw();
-    playArr[0].x +=   (Math.floor(Math.random() * 6) + 1 )* w;
-    if(playArr[0].x>(2*window.innerWidth/3)-w){ 
-        w=-w;
-        playArr[0].y -=h;
 
-        playArr[0].x-=playArr[0].x-(2*window.innerWidth/3);
+
+    playArr[0].x += ((ct % 6) + 1) * w;
+    ct++;
+
+
+    if ((Math.floor(playArr[0].x) > Math.floor((((2 * window.innerWidth / 3)) - Math.abs(w))))) {
+
+        playArr[0].y -= h;
+        w = -Math.abs(w);
+        playArr[0].x = ((2 * window.innerWidth / 3) - playArr[0].x + (2 * window.innerWidth / 3));
     }
-    else if(playArr[0].x<=((window.innerWidth/3))){ 
-        w=-w;
 
-        playArr[0].y -=h;
 
-       playArr[0].x=window.innerWidth/3+((window.innerWidth/3)-playArr[0].x);
+    else if (Math.floor(playArr[0].x) < Math.floor(((window.innerWidth / 3) + Math.abs(w)))) {
+        playArr[0].y -= h;
+        w = Math.abs(w);
+        playArr[0].x = (2 * window.innerWidth / 3) - playArr[0].x;
     }
-    if(playArr[0].y<window.innerHeight/4+h){
-        
+
+
+
+    if (Math.floor(playArr[0].y) > Math.floor((3 * window.innerHeight / 4))) {
+        playArr[0].y = (3 * window.innerHeight / 4);
+    }
+    else if (Math.floor(playArr[0].y) < Math.floor((window.innerHeight / 4 + h))) {
+        playArr[0].y = (window.innerHeight / 4) + h;
+    }
+
+
+
+    if (((Math.floor(playArr[0].y) == Math.floor(((window.innerHeight / 4) + h))) && (Math.floor(playArr[0].x) == Math.floor(((window.innerWidth / 3) + Math.abs(w)))))) {
+
         c.fillStyle = "papayawhip";
-        c.fillRect(window.innerWidth/2-window.innerWidth/12,3*window.innerHeight/4+window.innerHeight/20,window.innerWidth/6,window.innerHeight/10);
-        // c.font="40px";
+        c.fillRect(window.innerWidth / 2 - window.innerWidth / 12, 3 * window.innerHeight / 4 + window.innerHeight / 20, window.innerWidth / 6, window.innerHeight / 10);
+
         c.fillStyle = "black";
         c.textAlign = "center";
-        c.fillText("YOU WON",window.innerWidth/2,3*window.innerHeight/4+window.innerHeight/10);
-    }
-    
-        // +=   (Math.floor(Math.random() * 6) + 1)*h;
-        if(!(playArr[0].y<window.innerHeight/4+h)){
+        c.fillText("YOU WON", window.innerWidth / 2, 3 * window.innerHeight / 4 + window.innerHeight / 10);
+
+
+
+        playArr[0].x = ((window.innerWidth / 3) + Math.abs(w));
+        playArr[0].y = (window.innerHeight / 4) + h;
+
         playArr[0].draw();
-        }
+        disable();
 
-    console.log(playArr[0]);
+    }
+
+    else if ((Math.floor(playArr[0].y) > Math.floor(window.innerHeight / 4)) && (Math.floor(playArr[0].x) > Math.floor(((window.innerWidth / 3))))) {
+        playArr[0].draw();
+
+
+    }
+
+
+
+
+    outcome();
 }
-    // 
 
 
+// 
 
+var out = ["#", "#", "#", "##", "###", "###", "###",];
+function outcome() {
+    var outi = Math.floor(Math.random() * 256);
+
+    if ((out[outi % out.length] === "#") && ((playArr[0].y < (3 * window.innerHeight / 4 - h)) && (playArr[0].x > ((window.innerWidth / 3) + Math.abs(w))) && (playArr[0].x < ((2 * window.innerWidth / 3) - Math.abs(w))))) {
+
+
+        document.getElementById("result").innerText = "ALERT SNAKE!!";
+        c.beginPath();
+        c.strokeStyle = "red";
+        c.moveTo(playArr[0].x + w / 3, playArr[0].y - (2 * h / 3));
+        playArr[0].x -= (w);
+        playArr[0].y += h;
+        c.lineTo(playArr[0].x + w / 3, playArr[0].y - (2 * h / 3));
+        playArr[0].draw();
+        c.stroke();
+        w = -w;
+
+    }
+    else if ((out[outi % out.length] === "##") && ((playArr[0].y > (window.innerHeight / 4 + 2 * h)) && (playArr[0].x > ((window.innerWidth / 3) + Math.abs(w))) && (playArr[0].x < ((2 * window.innerWidth / 3) - Math.abs(w))))) {
+
+        document.getElementById("result").innerText = "YAY!! LADDER";
+        c.beginPath();
+        c.strokeStyle = "black";
+        c.moveTo(playArr[0].x + w / 3, playArr[0].y - (2 * h / 3));
+        playArr[0].x += (w);
+        playArr[0].y -= h;
+        c.lineTo(playArr[0].x + w / 3, playArr[0].y - (2 * h / 3));
+        playArr[0].draw();
+        c.stroke();
+        w = -w;
+
+    }
+
+
+    else {
+        document.getElementById("result").innerText = "PHEW!";
+
+    }
+
+}
